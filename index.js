f02b86325d9bf31522086b1c190cde2ccb8c03ef
Sasha <oleksandr@status.im>
2024-11-14 14:32:00 +0700

    up logs, awaits

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import { createLightNode, createDecoder, createEncoder, utf8ToBytes, bytesToUtf8 } from "@waku/sdk";
 
 (async () => {
-  const node = await createLightNode({ defaultBootstrap: true });
+  const node = await createLightNode({
+    defaultBootstrap: true,
+  });
   const decoder = createDecoder("/devcon/1/ping-pong/proto");
   const encoder = createEncoder({ contentTopic: "/devcon/1/ping-pong/proto" });
 
+  window.waku = node;
+
+  console.log("DEBUG: starting");
+  await node.start();
+
+  console.log("DEBUG: waiting for peers");
   await node.waitForPeers();
 
   const result = await node.filter.subscribe(decoder, (message) => {
@@ -15,10 +23,13 @@ import { createLightNode, createDecoder, createEncoder, utf8ToBytes, bytesToUtf8
 
   for (let i = 0; i < 10; i++) {
     console.log("DEBUG: waiting");
+
     await (new Promise(resolve => setTimeout(resolve, 5000)));
     console.log("DEBUG: sending message");
-    await node.lightPush.send(encoder, {
-      payload: utf8ToBytes("hey " + i)
+
+    const response = await node.lightPush.send(encoder, {
+      payload: utf8ToBytes("hey from " + node.peerId.toString() + " with index " + i)
     });
+    console.log("DEBUG: lightPush result ", response);
   }
 })();
